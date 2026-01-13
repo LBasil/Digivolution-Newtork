486233174073da14b5a3c379db48030e824637f4
Basil L'HÔTE <blhote@ankama.com>
2026-01-13 15:39:32 +0100

    Added all babies

--- data.js
+++ data.js
@@ -9,5 +9,15 @@ const DIGIMONS = {
   gabumon: { name:"Gabumon", stage:"Rookie", evolvesTo:["garurumon"], method:"Speed + Bond" },
   greymon: { name:"Greymon", stage:"Champion", evolvesTo:[], method:"—" },
   tyrannomon: { name:"Tyrannomon", stage:"Champion", evolvesTo:[], method:"Heavy weight" },
-  garurumon: { name:"Garurumon", stage:"Champion", evolvesTo:[], method:"High speed" }
+  garurumon: { name:"Garurumon", stage:"Champion", evolvesTo:[], method:"High speed" },
+  punimon: { name:"Punimon", stage:"Baby", evolvesTo:[], method:"Time" },
+  poyomon: { name:"Poyomon", stage:"Baby", evolvesTo:[], method:"Time" },
+  pupoyomonnimon: { name:"Poyomon", stage:"Baby", evolvesTo:[], method:"Time" },
+  yukimiBotamon: { name:"YukimiBotamon", stage:"Baby", evolvesTo:[], method:"Time" },
+  pabumon: { name:"Pabumon", stage:"Baby", evolvesTo:[], method:"Time" },
+  jyarimon: { name:"Jyarimon", stage:"Baby", evolvesTo:[], method:"Time" },
+  zerimon: { name:"Zerimon", stage:"Baby", evolvesTo:[], method:"Time" },
+  kuramon: { name:"Kuramon", stage:"Baby", evolvesTo:[], method:"Time" },
+  pichimon: { name:"Pichimon", stage:"Baby", evolvesTo:[], method:"Time" },
+  yuramon: { name:"Yuramon", stage:"Baby", evolvesTo:[], method:"Time" }
 };
